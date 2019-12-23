928de949ab8aea037b3abad1b4cbba7c0eb09024
Saurabh Yadav <49230235+saurabhyadav1985@users.noreply.github.com>
2019-12-23 17:32:12 +0530

    validations
    validations

--- ppmtool/src/main/java/org/sy/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/org/sy/ppmtool/web/ProjectController.java
@@ -3,7 +3,8 @@ package org.sy.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.sy.ppmtool.domain.Project;
 import org.sy.ppmtool.services.ProjectService;
 
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("api/project")
+@RequestMapping("/api/project")
 public class ProjectController {
 
     @Autowired
     public ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if (result.hasErrors()){
+            Map<String, String> errorMap = result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        
         Project project1 = projectService.saveOfUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
