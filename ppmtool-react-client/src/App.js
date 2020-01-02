28e412bacc21d0ad72fa95bfebb12162e8cd5cf8
Saurabh Yadav <49230235+saurabhyadav1985@users.noreply.github.com>
2020-01-02 09:45:37 +0530

    first components in react app
    first components in react app

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,26 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import ProjectItem from "./components/project/ProjectItem";
+import Header from "./components/layout/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to PPMTool!
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
