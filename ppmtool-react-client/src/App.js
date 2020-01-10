a8aaa14d2aa58a77067bc464a85b6439985388b2
Saurabh Yadav <syadav@thinkanalytics.com>
2020-01-10 10:18:50 +0530

    Update project form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import UpdateProject from "./components/project/UpdateProject";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
           </div>
         </Router>
       </Provider>
