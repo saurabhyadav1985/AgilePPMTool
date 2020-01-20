c1fe1a03b1e66e80677e5974ca290d9e896e4f52
Saurabh Yadav <syadav@thinkanalytics.com>
2020-01-20 16:18:25 +0530

    delete project front end and refactoring

--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS, GET_PROJECT } from "../actions/types";
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
 import { bindActionCreators } from "redux";
 
 const initialState = {
@@ -18,6 +18,14 @@ export default function(state = initialState, action) {
       return {
         ...state,
         project: action.payload
+      };
+
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(
+          project => project.projectIdentifier !== action.payload
+        )
       };
     default:
       return state;
